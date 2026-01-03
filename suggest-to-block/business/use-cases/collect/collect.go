ca344551dc228d0a5311af3047937d5fd1e24095
alextorq <morayk95@gmail.com>
2026-01-03 17:33:25 +0300

    feat: refactor domain handling to use a new database layer and improve event management

--- suggest-to-block/business/use-cases/collect/collect.go
+++ suggest-to-block/business/use-cases/collect/collect.go
@@ -1,7 +1,5 @@
 package collect
 
-import "github.com/alextorq/dns-filter/suggest-to-block/db"
-
 func CollectSuggest(blockedDomains []string, allowedDomains []string) {
 	for _, blockedDomain := range blockedDomains {
 		for _, allowedDomain := range allowedDomains {

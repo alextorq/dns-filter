eef568964f7a8107111af21c624d7cf7c9eeeb06
alextorq <morayk95@gmail.com>
2026-02-19 19:39:22 +0300

    add ui

---
+++ web/front/app/pages/clients/components/add-client-modal.vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+import {api} from '~/api'
+import {useToggle} from "~~/composables/use-toggle";
+import {useComponentStatus} from "~~/composables/use-component-status";
+import {ComponentStatus} from "~~/utils/component-status";
+import {getErrorMessage} from "~~/utils/get-error-message";
+
+const emit = defineEmits(['success'])
+
+const toast = useToast()
+const {isActive, closeHandler, openHandler} = useToggle()
+const {status} = useComponentStatus()
+
+
+const validationError = ref('')
+
+const state = reactive({
+  user_id: 0
+})
+
+const validation = (): string | null => {
+  if (state.user_id <= 0) {
+    return 'User ID must be a positive number.'
+  }
+  return null
+}
+
+const validateUserId = () => {
+  validationError.value = validation() || ''
+  return !validationError.value
+}
+
+const onSubmit = async (e: Event) => {
+  if (validateUserId()) {
+    try {
+      status.value = ComponentStatus.LOADING
+      e.preventDefault()
+      await api.addExcludeClient({ user_id: state.user_id })
+      toast.add({
+        title: 'Success',
+        description: 'New client was added.',
+        duration: 3000,
+      })
+      emit('success')
+      closeHandler()
+    }catch (e) {
+      status.value = ComponentStatus.ERROR_LOADING
+      console.error('Error', e)
+      const message = getErrorMessage(e)
+      toast.add({
+        title: 'Error',
+        description: message,
+        duration: 5000,
+        color: 'error',
+      })
+    }
+  }
+
+}
+</script>
+
+<template>
+  <UDrawer
+      v-model:open="isActive"
+      direction="right">
+    <UButton
+        size="xl"
+        @click="openHandler"
+        label="Add Client"
+        />
+
+    <template #header>
+      <h1>Add Client</h1>
+    </template>
+
+    <template #body>
+      <div>
+        <UForm
+            style="width: 600px;"
+            @submit="onSubmit"
+            :state="state"
+            >
+
+          <UFormField
+              label="User ID"
+              name="user_id"
+              required>
+            <UInput
+                size="xl"
+                type="number"
+                @change="validateUserId"
+                v-model="state.user_id"
+                placeholder="123"
+                required />
+
+            <div v-if="validationError">
+              {{validationError}}
+            </div>
+
+          </UFormField>
+        </UForm>
+
+      </div>
+    </template>
+
+    <template #footer>
+      <div class="flex justify-start">
+        <UButton @click="onSubmit" size="xl" label="Add Client" type="submit" />
+      </div>
+    </template>
+
+  </UDrawer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ web/front/app/pages/clients/index.vue
@@ -0,0 +1,134 @@
+<script setup lang="ts">
+import type { TableColumn } from '@nuxt/ui'
+import {api, type ExcludeClient} from "~/api";
+import {useComponentStatusWithLoading} from "~~/composables/use-component-status-with-loading";
+import {getErrorMessage} from "~~/utils/get-error-message";
+import AddClientModal from "~/pages/clients/components/add-client-modal.vue";
+
+const toast = useToast()
+
+let lastFetchController: AbortController | null = null
+
+const data = ref<ExcludeClient[]>([])
+
+const {isLoading, createLoadingRequest} = useComponentStatusWithLoading()
+
+useHead({
+  title: 'Exclude Clients',
+})
+
+const pagination = ref({
+  pageIndex: 0,
+  pageSize: 12,
+  total: 0,
+})
+
+const fetchData = async () => {
+  try {
+    if (lastFetchController) lastFetchController.abort()
+    lastFetchController = new AbortController()
+    const response = await api.getAllExcludeClients({
+      limit: pagination.value.pageSize,
+      offset: pagination.value.pageIndex * pagination.value.pageSize || 0,
+    }, lastFetchController.signal)
+
+    data.value = response.list
+    pagination.value = {
+      ...pagination.value,
+      total: response.total,
+    }
+  } catch (error) {
+    const message = getErrorMessage(error)
+    toast.add({
+      title: 'Error',
+      description: message,
+      duration: 5000,
+      color: 'error',
+    })
+    console.error('Error fetching data:', error)
+  }
+}
+
+const fetchWithLoading = createLoadingRequest(fetchData)
+
+const changePage = async (page: number) => {
+  pagination.value.pageIndex = page - 1
+  await fetchWithLoading()
+}
+
+onMounted(fetchWithLoading)
+
+const columns: TableColumn<ExcludeClient>[] = [
+  {
+    accessorKey: 'id',
+    header: 'ID',
+  },
+  {
+    accessorKey: 'user_id',
+    header: 'User ID',
+  },
+  {
+    accessorKey: 'created_at',
+    header: 'Created At',
+    cell: ({ row }) => {
+      return new Date(row.getValue('created_at')).toLocaleString('en-En', {
+        day: 'numeric',
+        month: 'short',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      })
+    }
+  },
+  {
+    accessorKey: 'updated_at',
+    header: 'Updated At',
+    cell: ({ row }) => {
+      return new Date(row.getValue('updated_at')).toLocaleString('en-En', {
+        day: 'numeric',
+        month: 'short',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      })
+    }
+  },
+  {
+    accessorKey: 'active',
+    header: 'Active',
+    cell: ({ row }) => {
+      return row.getValue('active') ? 'Yes' : 'No'
+    }
+  }
+]
+
+</script>
+
+<template>
+  <UContainer>
+    <div class="w-full space-y-4 pb-4">
+      <div class="flex justify-end">
+        <AddClientModal @success="fetchWithLoading" />
+      </div>
+
+      <UTable
+          :loading="isLoading"
+          empty="No data"
+          v-model:pagination="pagination"
+          :data="data"
+          :columns="columns"
+          class="flex-1"
+      />
+
+      <div class="flex justify-center border-t border-default pt-4">
+        <UPagination
+            :default-page="(pagination.pageIndex) + 1"
+            :items-per-page="pagination.pageSize"
+            :total="pagination.total"
+            @update:page="changePage"
+        />
+      </div>
+    </div>
+  </UContainer>
+
+</template>
